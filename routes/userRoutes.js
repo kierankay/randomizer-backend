0eac3c36455ea4bb3ae154d2c00efb1e5c6174f8
Kieran Kay <kierankay@gmail.com>
2019-10-26 18:31:41 -0700

    updated model source

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,10 +1,9 @@
 const express = require('express');
 const router = express.Router();
 const passport = require('passport')
-const { User } = require('../functions.js')
+const User = require('../models/User');
 
-
-router.post('/create', async function (req, res, next) {
+router.post('/', async function (req, res, next) {
   try {
     let { username, email, password } = req.body;
     let userDetails = await User.createUser(username, email, password);
@@ -25,6 +24,7 @@ router.get('/check', User.verifyJwt, async function (req, res, next) {
 
 router.post('/login', passport.authenticate('local', { failureRedirect: '/last-groups' }), async function (req, res, next) {
   try {
+    console.log('error')
     let { username, password } = req.body;
     let token = await User.loginUser(username, password);
     return res.json({ token });
