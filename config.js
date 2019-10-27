96b2cdbd41306798b003240b3c2368dc4b9d9f51
Kieran Kay <kierankay@gmail.com>
2019-10-27 16:39:16 -0700

    update PG config to use .env

--- config.js
+++ config.js
@@ -1,11 +1,10 @@
-let DB_URI = `postgresql://`;
 let NUM_ROUNDS = 12;
 let SECRET_KEY = 'somethingSecret'
 
 if (process.env.NODE_ENV === "test") {
-  DB_URI = `${DB_URI}/randomizer-test`
+  DB_URI = `randomizer-test`
 } else {
-  DB_URI = process.env.DATABASE_URL || `${DB_URI}/randomizer`;
+  DB_URI = process.env.DATABASE_URL || `randomizer`;
 }
 
 module.exports =  {DB_URI, NUM_ROUNDS, SECRET_KEY}
--- db.js
+++ db.js
@@ -1,7 +1,7 @@
-const {Client} = require('pg');
-const {DB_URI} = require('./config');
+const { Client } = require('pg');
+const { DB_URI } = require('./config');
 
-const db = new Client(DB_URI);
+const db = new Client({ database: DB_URI, user: process.env.PGUSER, password: process.env.PGPASSWORD });
 
 db.connect();
 
