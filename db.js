0f798c93acce3af70538799f92d535180f7931db
Kieran Kay <kierankay@gmail.com>
2019-10-27 17:23:22 -0700

    add dotenv

--- db.js
+++ db.js
@@ -1,5 +1,7 @@
 const { Client } = require('pg');
 const { DB_URI } = require('./config');
+const dotenv = require('dotenv');
+dotenv.config();
 
 const db = new Client({ database: DB_URI, user: process.env.PGUSER, password: process.env.PGPASSWORD });
 
