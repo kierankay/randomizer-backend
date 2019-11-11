f4ac49c1691f69ca23eaf18bf8163cb3f6e6f031
Kieran Kay <kierankay@gmail.com>
2019-11-11 00:46:52 -0800

    update config.js to fetch variables from .env

--- config.js
+++ config.js
@@ -1,5 +1,27 @@
+const dotenv = require('dotenv');
+dotenv.config();
+
+let {
+  SERVER,
+  PGUSER,
+  PGPASSWORD,
+  SECRET,
+  SESUSER,
+  SESPASSWORD
+} = process.env
+
 let NUM_ROUNDS = 12;
-let SECRET_KEY = 'somethingSecret'
+let SECRET_KEY = SECRET;
+
+TRANSPORTER = {
+  host: 'email-smtp.us-west-2.amazonaws.com',
+  port: 465,
+  secure: true,
+  auth: {
+    user: SESUSER,
+    pass: SESPASSWORD
+  }
+}
 
 if (process.env.NODE_ENV === "test") {
   DB_URI = `randomizer-test`
@@ -7,4 +29,4 @@ if (process.env.NODE_ENV === "test") {
   DB_URI = process.env.DATABASE_URL || `randomizer`;
 }
 
-module.exports =  {DB_URI, NUM_ROUNDS, SECRET_KEY}
+module.exports =  {DB_URI, NUM_ROUNDS, SECRET_KEY, TRANSPORTER, PGUSER, PGPASSWORD, SERVER}
