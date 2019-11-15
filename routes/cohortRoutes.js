f83ecd524596da472443bab657e0e306f5e7a503
Kieran Kay <kierankay@gmail.com>
2019-11-14 17:25:36 -0800

    update cohortRoutes.js GET and POST to fetch and pass in an orgId

--- routes/cohortRoutes.js
+++ routes/cohortRoutes.js
@@ -4,27 +4,30 @@ const Cohort = require('../models/Cohort');
 const Student = require('../models/Student');
 
 const router = express.Router();
+router.use(User.verifyJwt)
 
 router.get('/', async function (req, res, next) {
   try {
-    let result = await Cohort.getCohorts()
+    let orgId = req.user.organization;
+    let result = await Cohort.getCohorts(orgId);
     return res.json(result);
   } catch (err) {
     return next(err);
   }
 });
 
-router.post('/', User.verifyJwt, async function (req, res, next) {
+router.post('/', async function (req, res, next) {
   try {
     let { cohort } = req.body;
-    let added = await Cohort.addCohort(cohort);
+    let orgId = req.user.organization;
+    let added = await Cohort.addCohort(cohort, orgId);
     return res.json(added);
   } catch (err) {
     return next(err);
   }
 });
 
-router.get('/:id/students', User.verifyJwt, async function(req, res, next) {
+router.get('/:id/students', async function(req, res, next) {
   try {
     let students = await Student.getStudentsFromCohort(req.params.id)
     return res.json(students);
