f803a51702fdf75e59f4f87ba28a12ce66e46f64
Kieran Kay <kierankay@gmail.com>
2019-11-14 17:20:04 -0800

    Update Cohort component to consume org Id number

--- models/Cohort.js
+++ models/Cohort.js
@@ -2,20 +2,21 @@ const db = require('../db');
 
 class Cohort {
 
-  static async getCohorts() {
+  static async getCohorts(orgId) {
     let result = await db.query(`
       SELECT *
       FROM cohorts
-    `)
+      WHERE organization_id = $1
+    `, [orgId])
     return result.rows;
   }
   
-  static async addCohort(cohort) {
+  static async addCohort(cohort, orgId) {
     let result = await db.query(`
       INSERT INTO cohorts
-      (cohort_name)
-      VALUES ($1)
-      RETURNING cohort_name`, [cohort]
+      (cohort_name, organization_id)
+      VALUES ($1, $2)
+      RETURNING id, cohort_name`, [cohort, orgId]
     );
     return result.rows[0];
   }
