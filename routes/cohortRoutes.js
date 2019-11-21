5b2d3d46ce0d5faccbd29d49c1858bfb5d579c33
Kieran Kay <kierankay@gmail.com>
2019-11-21 11:10:12 -0800

    update cohortRoutes - nests groupRoutes and studentRoutes to be more RESTful
    Because groups and students all pertain to a particular cohort, it made sense to nest them inside of cohort routes to adhere to RESTful architectural conventions. Also added comments to describe each route

--- routes/cohortRoutes.js
+++ routes/cohortRoutes.js
@@ -1,10 +1,21 @@
+// Cohort routes include nested group and student routes
+
 const express = require('express');
 const User = require('../models/User');
 const Cohort = require('../models/Cohort');
 const Student = require('../models/Student');
+const Pair = require('../models/Pair');
+const { randomizePairs } = require('../helpers/helpers');
 
 const router = express.Router();
 router.use(User.verifyJwt)
+
+// COHORT ROUTES
+
+/*
+  Fetch all cohorts for logged in user's org
+  ex: GET /api/cohorts/
+*/
 
 router.get('/', async function (req, res, next) {
   try {
@@ -15,6 +26,15 @@ router.get('/', async function (req, res, next) {
     return next(err);
   }
 });
+
+/*
+  Create a new cohort for logged in users's org
+  ex: POST /api/cohorts/
+  body: {
+    cohort: "r13"
+  }
+*/
+
 
 router.post('/', async function (req, res, next) {
   try {
@@ -27,13 +47,98 @@ router.post('/', async function (req, res, next) {
   }
 });
 
+// COHORT > GROUP ROUTES
+
+/*
+  Fetch up to "limit" number of groups from a given cohort
+  ex: GET /api/cohorts/1/groups
+*/
+
+router.get('/:id/groups', async function (req, res, next) {
+  try {
+    let cohortId = req.params.id;
+    let { limit } = req.query;
+    let result = await Pair.getLastPairs(limit, cohortId)
+    return res.json(result);
+  } catch (err) {
+    return next(err);
+  }
+});
+
+/*
+  Generate (but do not save) a new group of randomized students from the cohort
+  Users may hit this API multiple times they are satisfied with student pairs before saving
+  ex: GET /api/cohorts/1/groups/random
+*/
+
+router.get('/:id/groups/random', async function (req, res, next) {
+  try {
+    let cohortId = req.params.id;
+    let { min_paired_ago } = req.query;
+    let list = await Student.getStudentsFromCohort(cohortId)
+    let pairs = await randomizePairs(list, min_paired_ago)
+    return res.json(pairs)
+  } catch (err) {
+    return next(err);
+  }
+});
+
+/*
+  Save a new group of randomized students from the cohort
+  ex: POST /api/cohorts/1/groups
+  body: {
+    project: "First group project",
+    group:
+  }
+*/
+
+router.post('/:id/groups', async function (req, res, next) {
+  try {
+    let cohortId = req.params.id;
+    let { group, project } = req.body;
+    let acceptedPairs = await Pair.acceptPairs(group, project, cohortId)
+    return res.json(acceptedPairs)
+  } catch (err) {
+    return next(err);
+  }
+});
+
+// COHORT > STUDENT ROUTES
+
+/* 
+  Fetch all students from a specific cohort
+  ex: GET /api/cohorts/1/students
+*/
+
 router.get('/:id/students', async function(req, res, next) {
   try {
-    let students = await Student.getStudentsFromCohort(req.params.id)
+    let cohortId = req.params.id;
+    let students = await Student.getStudentsFromCohort(cohortId)
     return res.json(students);
   } catch (err) {
     return next(err);
   }
-}); 
+});
+
+/*
+  Add a student to a cohort
+  ex: POST /api/cohorts/1/students
+  body {
+    first_name: "Kieran",
+    last_name: "Kay"
+  }
+*/
+
+router.post('/:id/students', async function (req, res, next) {
+  try {
+    let cohortId = req.params.id;
+    let student = req.body;
+    student.cohort = cohortId;
+    let result = await Student.addStudent(student);
+    return res.json(result);
+  } catch (err) {
+    return next(err);
+  }
+});
 
 module.exports = router;
