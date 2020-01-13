dec7fa8e601a540b63ef1d1a7c0095ea21eab7f3
kierankay <kierankay@gmail.com>
2020-01-12 19:07:46 -0800

    refactor helpers.js - split randomizePairs function into efficient, testable component functions

---
+++ __tests__/integration/helpers.test.js
@@ -0,0 +1,34 @@
+const randomizePairs = require('../../helpers/helpers');
+
+let students = [
+    {
+        id: 1,
+        first_name: Kieran,
+        last_name: Kay,
+        cohort_id: 1
+    },
+    {
+        id: 2,
+        first_name: Elizabeth,
+        last_name: Wang,
+        cohort_id: 1
+    },
+    {
+        id: 3,
+        first_name: Johnny,
+        last_name: Depp,
+        cohort_id: 1
+    },
+    {
+        id: 4,
+        first_name: Aylin,
+        last_name: Zafar,
+        cohort_id: 1
+    },
+    {
+        id: 5,
+        first_name: Tarron,
+        last_name: Kay,
+        cohort_id: 1
+    },
+]
--- helpers/helpers.js
+++ helpers/helpers.js
@@ -17,13 +17,34 @@ async function randomizePairs(studentsList, minRepeatDistance, cohort) {
   // adjacency matrix
   let newToOldMap = [];
   let oldToNewMap = [];
+
   for (let i = 0; i < studentsList.length; i++) {
     newToOldMap[i] = studentsList[i];
     oldToNewMap[studentsList[i].id] = i;
   }
 
+  let adjMatrix = await createAdjMatrix(studentsList.length, oldToNewMap, minRepeatDistance, cohort);
+  let recentGroup = await getRecentGroup(cohort);
+  let adjList = createAdjList(adjMatrix, recentGroup, minRepeatDistance);
+  let shuffledAdjList = shuffleAdjList(adjList);
+  let pairs = createPairs(shuffledAdjList, studentsList.length);
+  // Rebuild the pairs using the first map we created from new to old indexes
+
+  let rebuiltPairs = [];
+  for (let pair of pairs) {
+    let mappedStudent1 = newToOldMap[pair[0]];
+    let mappedStudent2 = newToOldMap[pair[1]];
+    let newPair = {
+      student_1: mappedStudent1,
+      student_2: mappedStudent2
+    };
+    rebuiltPairs.push(newPair);
+  }
+  return rebuiltPairs;
+}
+
+async function createAdjMatrix(studentCount, oldToNewMap, minRepeatDistance, cohort) {
   // create a new n * n empty adjacency matrix
-  let studentCount = studentsList.length;
   let adjMatrix = new Array(studentCount);
   for (let i = 0; i < studentCount; i++) {
     adjMatrix[i] = new Array(studentCount);
@@ -35,20 +56,26 @@ async function randomizePairs(studentsList, minRepeatDistance, cohort) {
   let edges = await Pair.getPairsEdgeList(minRepeatDistance, cohort);
   for (let edge of edges) {
     let s1 = oldToNewMap[edge.student1_id];
-    let s2 = oldToNewMap[edge.student2_id] || s1;
+    let s2 = oldToNewMap[edge.student2_id] || s1; // for solo students
     let weight = edge.group_id;
     if (adjMatrix[s1][s2] === undefined) {
       adjMatrix[s1][s2] = weight;
       adjMatrix[s2][s1] = weight;
     }
   }
+  return adjMatrix;
+}
 
-  // Compute an adjacency list of the past pairs given the minPairDistance.
-  // Clear this whenever new pairs are created.
-
+async function getRecentGroup(cohort) {
+  let edges = await Pair.getPairsEdgeList(1, cohort);
   let recentGroup = edges[0] ? edges[0].group_id : null;
-  let adjList = new Array(adjMatrix.length);
+  return recentGroup;
+}
 
+function createAdjList(adjMatrix, recentGroup, minRepeatDistance) {
+  // Compute an adjacency list of the possible pairs given the minPairDistance.
+  // Clear this whenever new pairs are created.
+  let adjList = new Array(adjMatrix.length);
   for (let i = 0; i < adjMatrix.length; i++) {
     adjList[i] = [];
     for (let j = 0; j < adjMatrix[0].length; j++) {
@@ -57,78 +84,63 @@ async function randomizePairs(studentsList, minRepeatDistance, cohort) {
       }
     }
   }
+  return adjList;
+}
+
+function shuffleAdjList(adjList) {
 
   // Shuffle the adjacency list to introduce randomness.
-
   for (let i = 0; i < adjList.length; i++) {
     for (let j = 0; j < adjList[i].length; j++) {
-      let randIdx = Math.floor(Math.random()*adjList[i].length);
+      let randIdx = Math.floor(Math.random() * adjList[i].length);
       let temp = adjList[i][randIdx];
       adjList[i][randIdx] = adjList[i][j];
       adjList[i][j] = temp;
     }
   }
+  return adjList;
+}
 
+function createPairs(adjList, studentCount, start = 0, used = new Set(), pairs = []) {
   // Recursively find and return the first valid pair
   // INPUTS: list of students, pairs, set of used students
   // OUTPUTS: 
-
-  let used = new Set();
-  let pairs = [];
-
-  function createPairs(start) {
-    if (used.size === studentCount) {
-      return true;
-    }
-    for (let i = start; i < adjList.length; i++) {
-      if (!used.has(i)) {
-        for (let j = 0; j < adjList[i].length; j++) {
-          if (!used.has(adjList[i][j])) {
-            if (i === adjList[i][j]) {
-              if ((studentCount - used) % 2 === 1 || studentCount === 1) {
-                pairs.push([i]);
-                used.add(i);
-                if (createPairs(i + 1)) {
-                  return true;
-                } else {
-                  pairs.pop();
-                  used.delete(i);
-                }
-              }
-            } else {
-              pairs.push([i, adjList[i][j]]);
-              used.add(i) 
-              used.add(adjList[i][j]);
-              if (createPairs(i + 1)) {
-                return true;
+  if (used.size === studentCount) {
+    return pairs;
+  }
+  
+  for (let i = start; i < adjList.length; i++) {
+    if (!used.has(i)) {
+      for (let j = 0; j < adjList[i].length; j++) {
+        if (!used.has(adjList[i][j])) {
+          if (i === adjList[i][j]) {
+            if ((studentCount - used) % 2 === 1 || studentCount === 1) {
+              pairs.push([i]);
+              used.add(i);
+              if (createPairs(adjList, studentCount, i + 1, used, pairs)) {
+                return pairs;
               } else {
                 pairs.pop();
-                used.delete(i)
-                used.delete(adjList[i][j]);
+                used.delete(i);
               }
+            }
+          } else {
+            pairs.push([i, adjList[i][j]]);
+            used.add(i)
+            used.add(adjList[i][j]);
+            if (createPairs(adjList, studentCount, i + 1, used, pairs)) {
+              return pairs;
+            } else {
+              pairs.pop();
+              used.delete(i)
+              used.delete(adjList[i][j]);
             }
           }
         }
       }
     }
   }
-  createPairs(0);
-  
-  // Rebuild the pairs using the first map we created from new to old indexes
-  
-  let rebuiltPairs = [];
-
-  for (let pair of pairs) {
-    let mappedStudent1 = newToOldMap[pair[0]];
-    let mappedStudent2 = newToOldMap[pair[1]];
-    let newPair = {
-      student_1: mappedStudent1,
-      student_2: mappedStudent2
-    };
-    rebuiltPairs.push(newPair);
-  }
-
-  return rebuiltPairs;
+  return false;
 }
 
 module.exports = {
