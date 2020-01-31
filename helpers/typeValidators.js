0b147197bc97a93e21d5fbf6ffecb854f5dfb548
kierankay <kierankay@gmail.com>
2020-01-31 09:49:10 -0800

    update typeValidators.js - group functions into a class

--- helpers/typeValidators.js
+++ helpers/typeValidators.js
@@ -1,16 +1,16 @@
-function checkArray(arr, varName) {
-    if (!Array.isArray(arr)) {
-        throw new Error(`${studentsList} must be an array`);
+
+class typeValidation {
+    static checkArray(arr, varName) {
+        if (!Array.isArray(arr)) {
+            throw new Error(`${studentsList} must be an array`);
+        }
+    }
+
+    static isNum(num, numName) {
+        if (isNaN(num)) {
+            throw new Error(`${numName} must be an integer`);
+        }
     }
 }
 
-function checkNum(num, numName) {
-    if (isNaN(num)) {
-        throw new Error(`${numName} must be an integer`);
-    }
-}
-
-module.exports = {
-    checkArray,
-    checkNum
-}
+module.exports = typeValidation;
